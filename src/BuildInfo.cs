402d232c94558ec036c1444db0e67adfaf465420
Chris King <kingces95@gmail.com>
2016-09-24 12:37:13 -0700

    remove SubstituteExpandedResult

--- src/BuildInfo.cs
+++ src/BuildInfo.cs
@@ -1,9 +1,9 @@
 ﻿internal class BuildInfo {
-    internal const string Number = string.Empty;
-    internal const string EnlistmentRevision = string.Empty;
-    internal const string EnlistmentUrl = string.Empty;
-    internal const string EnslitmentBranch = string.Empty;
+    internal const string Number = "";
+    internal const string EnlistmentRevision = "";
+    internal const string EnlistmentUrl = "";
+    internal const string EnslitmentBranch = "";
 
-    internal const string MajorVersion = string.Empty;
-    internal const string MinorVersion = string.Empty;
+    internal const string MajorVersion = "";
+    internal const string MinorVersion = "";
 }
