06a45776228fa36e5aca9a53241631ed8d9bd4b0
Chris King <kingces95@gmail.com>
2016-09-24 22:42:59 -0700

    Debug myGet

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+echo Pre nuget restore
 nuget restore -Source https://chocolatey.org/api/v2
+echo Post nuget restore
 pushd %~dp0
 packages\git-lfx.0.1.0\tools\git-lfx checkout
 msbuild src\mxbuild\shim\shim.proj /v:m /t:build /p:Configuration="release"
