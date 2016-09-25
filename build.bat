90242df4657267721c057577df0e54dc92f84b35
Chris King <kingces95@gmail.com>
2016-09-24 22:15:06 -0700

    build.bat only release

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 nuget restore -Source https://chocolatey.org/api/v2
 pushd %~dp0
 packages\git-lfx.0.1.0\tools\git-lfx checkout
-msbuild src\mxbuild\shim\shim.proj /v:m /t:build /p:Configuration="debug;release"
+msbuild src\mxbuild\shim\shim.proj /v:m /t:build /p:Configuration="release"
 popd
