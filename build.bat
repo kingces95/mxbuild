2d3ceb0f633c9f9de06da926fd724e321046ba26
Chris King <kingces95@gmail.com>
2016-09-24 19:24:27 -0700

    retry MyGit build

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 nuget restore -Source https://chocolatey.org/api/v2
-set path=%path%;%~dp0packages\git-lfx.0.1.0\tools\
-git lfx checkout
-msbuild %~dp0src\mxbuild\shim\shim.proj /v:m /t:build /p:Configuration="debug;release"
+pushd %~dp0
+packages\git-lfx.0.1.0\tools\git-lfx checkout
+msbuild src\mxbuild\shim\shim.proj /v:m /t:build /p:Configuration="debug;release"
+popd
