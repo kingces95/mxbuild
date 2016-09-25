5c58f9494c257ca375c7e24485896234949a4c26
Chris King <kingces95@gmail.com>
2016-09-24 22:48:19 -0700

    debug myGet

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 echo Pre nuget restore
 call nuget restore -Source https://chocolatey.org/api/v2
 echo Post nuget restore
+git --version
+for /F "tokens=*" %%i in ('git remote') do git remote get-url %%i
 pushd %~dp0
 packages\git-lfx.0.1.0\tools\git-lfx checkout
 msbuild src\mxbuild\shim\shim.proj /v:m /t:build /p:Configuration="release"
